b33da614045eb444b536bdcb270b6362120aa8a0
sailiks <ssengu15@asu.edu>
2018-08-28 18:10:56 -0700

    Adding UI templates and app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(exp=0, gs='Extinguish Big Fire At Byeng'):
+    p = {1:'lala'}
+    return render_template('index.html', canAskForExplanations=exp, plan=p)
+
+if __name__ == '__main__':
+    app.run(port=5000)
