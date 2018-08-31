aab8a715be715f34803349ba72414ff0d6f62113
Sachin Grover <sachin.grover@asu.edu>
2018-08-30 17:36:19 -0700

    interface tested, for integration check test function

--- interface.py
+++ interface.py
@@ -2,9 +2,9 @@ import json
 
 class Interface:
     def __init__(self, connector = "_"):
-        path = "problem_generator/config"
-        self.courses = self.get_data(path + "courses.json")
-        self.professors = self.get_data(path + "committee.json")
+        path = "problem_generator/config/"
+        self.courses = self.getData(path + "courses.json")
+        self.committee = self.getData(path + "committee.json")
         self.cache = dict()
         self.connector = connector
         self.num = ["ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN"]
@@ -19,8 +19,8 @@ class Interface:
             "Complete Foundations": "COMPLETE_FOUNDATIONS",
             "Complete Applications": "COMPLETE_APPLICATIONS",
             "Complete Systems": "COMPLETE_SYSTEMS",
-            "Thesis Credit A": "TAKE_CSE599A",
-            "Thesis Credit B": "TAKE_CSE599B"
+            "Thesis Course A": "TAKE_CSE599A",
+            "Thesis Course B": "TAKE_CSE599B"
         }
 
 
@@ -31,83 +31,136 @@ class Interface:
 
         return data
 
-    def uiToAction(self, ui_actions):
+    def uiToActions(self, ui_actions):
         self.course_counter = 0
         self.sem_counter = 0
         self.committee_counter = 1
+        self.committee_members = []
         actions = []
-        for action in ui_actions['plan']:
+        for action in ui_actions:
             if action['name'].find("-"):
                 act = action['name'].split("-")
                 act = [x.strip() for x in act]
-            else:
-                act = [action['name'].strip(), None]    
-            actions.push(self.__converter(act[0], act[1]))
+
+            actionType = act[0]
+            if act[0] == "Add":
+                if act[1].find("Semester") > -1:
+                    actionType = "Complete Semester"
+                else:
+                    actionType = "Defend"
+            actions.append(str(self.__converter(actionType, act[1])))
 
         return actions
 
     def __converter(self, action, name):
         switch = {
-            "Add Course": __addCourse,
-            "Add Chair": __addChair,
-            "Add Committee": __addCommittee
-            "Complete Semester": __endSemester
+            "Add Course": self.__addCourse,
+            "Add Chair": self.__addChairAndCommittee,
+            "Add Committee": self.__addChairAndCommittee,
+            "Complete Semester": self.__endSemester,
+            "Defend": self.__defend,
         }
 
-        func = switcher.get(action, __default)
+        func = switch.get(action, self.__default)
         return func(action, name)
 
-    def __addChair(self, action, name):
-        name = name.upper()
-        action = self.mapper[action] + self.connector + name
-        self.committee_members = [name] + self.committee_members
+    def __addChairAndCommittee(self, action, name):
+        prof, _ = self.__find(self.committee, name)
+        if prof is None:
+            return None
 
-    def __addCommittee(self, action, name):
-        if self.committee_counter < 3:
-            self.committee_counter += 1
-        name = name.upper()
-        action =    self.mapper[action] + self.connector + self.num[self.committee_counter] +
-                    self.connector + name
-        self.committee_membners.append(name)
+        prof = prof.upper()
+        action = self.mapper[action] + self.connector
+        if action == "Add Committee":
+            if self.committee_counter < 3:
+                self.committee_counter += 1
+            action += self.committee_counter + self.connector
+            self.committee_members.append(prof)
+        else:
+            self.committee_members = [prof] + self.committee_members
+
+        action += prof
         return action
 
     def __endSemester(self, action, name):
-        action = self.mapper[action] + self.connector + self.sem_counter
+        action = self.mapper[action] + self.connector + str(self.sem_counter)
         self.sem_counter = 0
         return action
+
+    def __defend(self, action, name):
+        return self.mapper[action] + self.connector + self.connector.join(self.committee_members)
 
     def __default(self, action, name):
         return self.mapper[action] + self.connector + name.upper()
 
     def __addCourse(self, action, name):
         if name in self.cache:
-            action =    self.mapper[self.cache[name][1]] + self.connector +
+            action =    self.mapper[self.cache[name][1]] + self.connector +\
                         self.cache[name][0] + self.connector
         else:
-            if name.find("Thesis"):
+            if name.find("Thesis") > -1:
+                name = self.__removeType(name)
                 action =    self.mapper[name] + self.connector
+                courseType = "research"
             else:
-                course, courseType = self.__getAction(name)
+                course, courseType = self.__find(self.courses, name)
                 if course is None:
-                    print "Course not found"
                     return None
                 self.cache[name] = [course, courseType]
-                action =    self.mapper[courseType] + self.connector + self.course + self.connector
+                if courseType == "deficiency":
+                    action =    self.mapper["deficiency"] + self.connector + course.upper() + self.connector
+                    action +=   self.num[10] + self.connector + self.num[10] + self.connector
+                else:
+                    action =    self.mapper["normal"]
+                    action +=   self.connector + course.upper() + self.connector
 
-        action +=   self.num[self.course_counter] + self.connector +
-                    self.num[self.course_counter + 1] + self.connector + 
-                    self.num[self.sem_counter] + self.connector +
-                    self.num[self.sem_counter + 1]
+            if courseType != "deficiency":
+                action +=   self.num[self.course_counter] + self.connector +\
+                            self.num[self.course_counter + 1] + self.connector
+                self.course_counter += 1
 
-        self.course_counter += 1
+            action +=       self.num[self.sem_counter] + self.connector +\
+                            self.num[self.sem_counter + 1]
+
         self.sem_counter += 1
 
         return action
 
 
-    def __getAction(self, name):
-        for key in self.courses.keys():
-            if self.courses[key][0] == name:
-                return key, self.courses[key][1]
-        
+    def __find(self, data, name):
+        name = self.__removeType(name)
+        for key in data.keys():
+            if data[key][0] == name:
+                return key, data[key][1]
+
+        print "No value found for ", name
         return None, None
+
+    def __removeType(self, name):
+        return name.split("(")[0].strip()
+
+def test():
+    plan =  [
+                {"name":"Add Course - Embedded Operating Systems Internals (systems)",
+                        "x": 0, "y": 0, "width": 12, "height": 1},
+                {"name": "Add Course - Software Project, Process and Quality Management (systems)",
+                        "x": 0, "y": 0, "width": 12, "height": 1},
+                {"name": "Add Course - Computer Organization and Assembly Language Programming (deficiency)",
+                        "x": 0, "y": 0, "width": 12, "height": 1},
+                {"name": "Add - End of Semester", "x": 0, "y": 1, "width": 12, "height": 1},
+                {"name": "Add Course - Software Verification, Validation and Testing (systems)",
+                        "x": 0, "y": 2, "width": 12, "height": 1},
+                {"name": "Add Committee - Guoliang Xue (foundations)",
+                        "x": 0, "y": 3, "width": 12, "height": 1},
+                {"name": "Add - End of Semester", "x": 0, "y": 4, "width": 12, "height": 1},
+                {"name": "Add Chair - Arunabha Sen (foundations)",
+                        "x": 0, "y": 5, "width": 12, "height": 1},
+                {"name": "Add Course - Thesis Course A (research)", "x": 0, "y": 4, "width": 12, "height": 1},
+                {"name": "Add - Defense", "x": 0, "y": 6, "width": 12, "height": 1}
+            ]
+
+    inter = Interface(" ")
+    print inter.uiToActions(plan)
+
+if __name__ == "__main__":
+    test()
