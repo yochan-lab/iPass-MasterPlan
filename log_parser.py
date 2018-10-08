bb812d08d33b7c0f72f0a1dc506fa97f48a61a6d
sachingrover211 <sachin.grover@asu.edu>
2018-10-08 00:06:39 -0700

    changes to parse information from ipos

--- log_parser.py
+++ log_parser.py
@@ -7,6 +7,9 @@ import pandas as pd
 class Parser:
     def __init__(self):
         # nothing to do here for now
+        # self.specialization_ai = ["CSE571", "CSE574", "CSE575"]
+        # self.specialization_bd = ["CSE510", "CSE512", "CSE572"]
+        # self.specialization_cs = ["CSE543", "CSE545", "CSE548"]
         self.dummy = None
 
     '''
@@ -15,13 +18,16 @@ class Parser:
     def parse(self, folderName):
         times, files = self.__get_files(folderName)
         data = self.__get_data(files, times)
+        data_copy = data.copy()
+        for index, row in data_copy.iterrows():
+            data["completion_ratio"] = self.__parse_ipos(row["final-IPOS"])
+
         with open("logs_data.csv", "w+") as f:
             data.to_csv(f, sep="\t", encoding="utf-8", index = False)
-        #for index, row in data.iterrows():
-            #parse
+
 
     '''
-    ' get all the log file names and their last modified 
+    ' get all the log file names and their last modified
     ' times from <folderName>\logs folder, in two lists
     '''
     def __get_files(self, folderName):
@@ -52,7 +58,57 @@ class Parser:
                     # merge data with the previous dataset
                     objs = pd.concat([objs, d])
 
+        objs = objs.sort_value("edit_time")
         return objs
+    '''
+    ' returns the ratio of actions completed to actual valid plan length of 21
+    '''
+    def __parse_ipos(self, ipos):
+        return min(1, len(ipos)/21)
+
+    '''
+    The idea is dropped
+    ' parses ipos which is in the form of actions to define
+    ' whether all deficiencies were taken - 0, 1 value
+    ' ten courses were completed - -1 (less than 10), 0 (10), 1(greater than 10)
+    ' thesis A was added - 0, 1 value
+    ' thesis B was added - 0, 1 value
+    ' defense was added - 0, 1 value
+    ' foundation, application, system were completed - 0, 1 value
+    ' were there three specialization courses - 0, 1 value
+    ' specialization was completed - 0, 1 value
+    ' is the plan valid and correct - 0, 1 value
+    def __parse_ipos(self, data, index):
+        status = {
+            "deficiency": False,
+            "ten_course": False,
+            "thesis_A": False,
+            "thesis_B": False,
+            "defense": False,
+            "concentration": False,
+            "specialization_courses": False,
+            "specialization": False,
+            "plan_valid": False
+        }
+
+        ui_action = {
+            "name": "",
+            "x": 0,
+            "y": 0,
+            "width": 12,
+            "height": 1
+        }
+
+        plan = data[index]["final-IPOS"]
+        fomatted_plan = []
+        for count, action in enumerate(plan):
+            temp = ui_action.copy()
+            temp["y"] = count
+            temp["name"] = action
+            formatted_plan.append(temp)
+
+        is_plan_complete =
+    '''
 
 if __name__ == "__main__":
     p = Parser()
